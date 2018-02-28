1b11bc2876db23d90d0ba60a1d5612fbc1fddba6
Rush-light <lightning_o-din0711@docomo.ne.jp>
2018-02-28 21:43:24 +0900

    0228

--- Webproj5/src/Com/internousdev/webproj5/action/LoginAction.java
+++ Webproj5/src/Com/internousdev/webproj5/action/LoginAction.java
@@ -3,13 +3,14 @@ package Com.internousdev.webproj5.action;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 import Com.internousdev.webproj5.dao.LoginDAO;
-import Com.internousdev.webproj5.dto.LoginDTO;;
+import Com.internousdev.webproj5.dto.LoginDTO;
 
 public class LoginAction extends ActionSupport implements SessionAware {
 
@@ -27,10 +28,10 @@ public class LoginAction extends ActionSupport implements SessionAware {
 		loginDTOList=dao.select(username, password);
 		if(this.username.equals(loginDTOList.get(0).getUsername())
 		&& this.password.equals(loginDTOList.get(0).getPassword())){
-			session.put("loginDTOList,loginDTOList");
+			session.put("loginDTOList",loginDTOList);
 			ret=SUCCESS;
 		}else{
-			session.put("loginDTOList,loginDTOList");
+			session.put("loginDTOList",loginDTOList);
 			ret=ERROR;
 		}
 		return ret;
@@ -41,5 +42,20 @@ public class LoginAction extends ActionSupport implements SessionAware {
 	}
 	public void setUsername(String username) {
 		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password=password;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.session=session;
 	}
 }
