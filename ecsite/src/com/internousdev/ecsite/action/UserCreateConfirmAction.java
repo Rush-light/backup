952666ce6cb187fdfee116ece5ce0ed54c41d1cc
Rush-light <lightning_o-din0711@docomo.ne.jp>
2018-03-14 21:47:30 +0900

    0314

--- ecsite/src/com/internousdev/ecsite/action/UserCreateConfirmAction.java
+++ ecsite/src/com/internousdev/ecsite/action/UserCreateConfirmAction.java
@@ -21,9 +21,56 @@ public class UserCreateConfirmAction  extends ActionSupport implements SessionAw
 		if(!(loginUserId.equals(""))
 				&&!(loginPassword.equals(""))
 				&&!(userName.equals(""))) {
+					session.put("loginUserId", loginUserId);
+					session.put("loginPassword", loginPassword);
+					session.put("userName", userName);
+		}else {
+			setErrorMassage("未入力の項目があります");
+			result=ERROR;
 
 		}
+		return result;
 
 	}
 
-}
+
+	public String getLoginUserId() {
+		return loginUserId;
+	}
+
+	public void setLoginUserId(String loginUserId) {
+		this.loginUserId=loginUserId;
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword=loginPassword;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName=userName;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session=session;
+	}
+
+	public String getErrorMassage() {
+		return errorMassage;
+	}
+
+	public void setErrorMassage(String errorMassage) {
+		this.errorMassage=errorMassage;
+	}
+	}
+
+
+
